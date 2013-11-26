3f8e295f97a7af6ea644a63cef4224345973c98c
George Cheteles <georgecheteles@reea.net>
2013-11-26 10:12:44 +0200

    ver. 1.4.7

--- jquery.isAlive.js
+++ jquery.isAlive.js
@@ -5,7 +5,7 @@
 | |/ |/ /  __/_____/ /__/ /_/ / /_/ /  __/_____/ / / / / / /_/ / /_/ / / /__  
 |__/|__/\___/      \___/\____/\__,_/\___/     /_/ /_/ /_/\__,_/\__, /_/\___/  
                                                               /____/          
-jQuery.isAlive(1.4.6)
+jQuery.isAlive(1.4.7)
 Written by George Cheteles (george@we-code-magic.com).
 Licensed under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license. 
 Please attribute the author if you use it.
@@ -1962,22 +1962,29 @@ Last modification on this file: 26 November 2013
 					}
 					
 					while((directionForward && step<=pos) || (!directionForward && step>=pos)){
+
 						/*ANIMATE-SET && SET && ADD && REMOVE CLASS*/
 						if(step==parseInt(step)){
-							(directionForward)?direction = 'forward':((step!=stepEnd)?direction = 'backward':direction = 'forward');
-							if(typeof(thisObj.setArray[direction][thisObj.getPos(step)])!="undefined"){ 
-								for(selector in thisObj.setArray[direction][thisObj.getPos(step)]){
-									for(property in thisObj.setArray[direction][thisObj.getPos(step)][selector])
-										thisObj.setCSS(selector,property,thisObj.setArray[direction][thisObj.getPos(step)][selector][property]);
+							//(directionForward)?direction = 'forward':((step!=stepEnd)?direction = 'backward':direction = 'forward');
+							(directionForward)?((step!=stepStart)?direction = 'forward':direction = null):((step!=stepEnd)?direction = 'backward':direction = null);
+							if(direction!=null){
+								
+								console.log(step);
+								
+								if(typeof(thisObj.setArray[direction][thisObj.getPos(step)])!="undefined"){ 
+									for(selector in thisObj.setArray[direction][thisObj.getPos(step)]){
+										for(property in thisObj.setArray[direction][thisObj.getPos(step)][selector])
+											thisObj.setCSS(selector,property,thisObj.setArray[direction][thisObj.getPos(step)][selector][property]);
+									}
 								}
-							}
-							if(typeof(thisObj.onOffClassArray[direction][thisObj.getPos(step)])!="undefined"){ 
-								for(selector in thisObj.onOffClassArray[direction][thisObj.getPos(step)]){
-									for(className in thisObj.onOffClassArray[direction][thisObj.getPos(step)][selector]){
-										if(thisObj.onOffClassArray[direction][thisObj.getPos(step)][selector][className]==true)
-											jQuery(selector).addClass(className);
-										else
-											jQuery(selector).removeClass(className);
+								if(typeof(thisObj.onOffClassArray[direction][thisObj.getPos(step)])!="undefined"){ 
+									for(selector in thisObj.onOffClassArray[direction][thisObj.getPos(step)]){
+										for(className in thisObj.onOffClassArray[direction][thisObj.getPos(step)][selector]){
+											if(thisObj.onOffClassArray[direction][thisObj.getPos(step)][selector][className]==true)
+												jQuery(selector).addClass(className);
+											else
+												jQuery(selector).removeClass(className);
+										}
 									}
 								}
 							}
@@ -2623,7 +2630,7 @@ Last modification on this file: 26 November 2013
 			return getBrowser();
 		},
 		getVersion : function(){
-			return "1.4.6";
+			return "1.4.7";
 		}
 	};
 	
