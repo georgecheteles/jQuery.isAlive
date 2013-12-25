34f8b207e8891013b6dbff1680700211a8c09d3d
georgecheteles <george@we-code-magic.com>
2013-12-25 16:53:28 +0200

    ver. 1.7.1

--- src/jquery.isAlive.js
+++ src/jquery.isAlive.js
@@ -5,14 +5,14 @@
 | |/ |/ /  __/_____/ /__/ /_/ / /_/ /  __/_____/ / / / / / /_/ / /_/ / / /__  
 |__/|__/\___/      \___/\____/\__,_/\___/     /_/ /_/ /_/\__,_/\__, /_/\___/  
                                                               /____/          
-jQuery.isAlive(1.7.0)
+jQuery.isAlive(1.7.1)
 Written by George Cheteles (george@we-code-magic.com).
 Licensed under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license. 
 Please attribute the author if you use it.
 Find me at:
 	http://www.we-code-magic.com 
-	office@we-code-magic.com
-Last modification on this file: 24 December 2013 (Merry Christmas)
+	george@we-code-magic.com
+Last modification on this file: 25 December 2013 (Merry Christmas)
 */
 
 (function(jQuery) {
@@ -275,7 +275,6 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 				/(msie) ([\w.]+)/.exec( ua ) ||
 				ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
 				[];
-
 			return {
 				browser: match[1] || "",
 				version: match[2] || "0"
@@ -284,10 +283,8 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 		matched = jQuery.uaMatch( navigator.userAgent );
 		browser = {};
 		var mobile = (/(android|bb\d+|meego).+mobile|webos|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(userAgent.substr(0,4)));
-		
 		if(mobile)
 			browser.mobile = true;
-		
 		if(matched.browser) {
 			browser[ matched.browser ] = true;
 			browser.version = matched.version;
@@ -409,16 +406,15 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 		this.params = {};
 		this.onComplete = null;
 		this.uniqId = Math.round(Math.random()*1000)+1;
-		this.CSS3DefaultTransitionArray = {};
-		this.CSS3TransitionArray = {};
-		this.CSS3ValuesArray = {};
 		this.functionsArray = {};
 		this.haveStepPoints;
 		this.rebuildOnStop = false;
-		this.lastElemValue = {};
 		this.dragHorizontal = null;
 		this.scrollBarPosition = 0;
-
+		this.CSS3DefaultTransitionArray = {};
+		this.CSS3TransitionArray = {};
+		this.CSS3ValuesArray = {};
+		this.JSValuesArray = {};
 		/* MY CLASS/SET ARRAYS */
 		this.setArray = {};
 		this.onOffClassArray = {};
@@ -517,9 +513,9 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 	/*ANIMATE FUNCTION THAT WORKS FOR NON JQUERY ANIMATED PROPERTIES*/
 	isAlive.prototype.animateJS = function(startPos,selector,property,value,duration,easing){
 		var thisObj = this;
-		if(startPos==parseInt(startPos))
-			thisObj.lastElemValue[selector+'|'+property] = thisObj.animPositions[thisObj.getPos(startPos)][selector][property].toString();
-		var start = thisObj.lastElemValue[selector+'|'+property];
+		if(typeof(thisObj.JSValuesArray[selector][property])=="undefined")
+			thisObj.JSValuesArray[selector][property] = thisObj.animPositions[thisObj.getPos(startPos)][selector][property].toString();
+		var start = thisObj.JSValuesArray[selector][property];
 		var end = value.toString();
 		var tempObj = {};
 		tempObj[property.replace(/-/g,"")+'Timer']="+=100";
@@ -527,7 +523,6 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 			step: function(step,fx){
 				var pos = step-fx.start;
 				var value = getAtPosValue(pos,start,end,0,100);
-				thisObj.lastElemValue[selector+'|'+property] = value;
 				thisObj.setCSS(selector,property,value);
 			}
 		});
@@ -537,6 +532,7 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 	isAlive.prototype.setCSS = function(selector,property,value){
 		var thisObj = this;
 		if(typeof(thisObj.functionsArray[property])!="undefined"){
+			thisObj.JSValuesArray[selector][property] = value;
 			thisObj.functionsArray[property](selector,value);
 			return;
 		}
@@ -557,12 +553,16 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 			delete thisObj.CSS3ValuesArray[selector][property];
 			jQuery(selector).css(vPTransition,thisObj.getTransitionArray(selector));
 		}
+		else if(typeof(thisObj.JSValuesArray[selector])!="undefined" && typeof(thisObj.JSValuesArray[selector][property])!="undefined")
+			thisObj.JSValuesArray[selector][property] = value;
+		
 		jQuery(selector).css(property,value);
 	}
 	
 	/* REPLACES PARAMS */
 	isAlive.prototype.convertParams = function(params,format){
 		var thisObj = this;
+		var stringPositions = {top:"0%",center:"50%",bottom:"100%",left:"0%",right:"100%"};
 		
 		if(typeof(params)!="function"){
 			params = params.toString();
@@ -617,12 +617,14 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 			
 			return params;
 		}
-		var stringPositions = {top:"0%",center:"50%",bottom:"100%",left:"0%",right:"100%"};
+		
 		params = doRecursive(fixSpaces(params));
+		
 		if(typeof(format)!='undefined')
 			params = format.replace('(X)','('+params+')').replace('Xpx',params+'px').replace('X%',params+'%').replace('Xdeg',params+'deg').replace('Xem',params+'em');
 		if(isNumber(params))
 			return parseFloat(params);
+			
 		return params;
 	}
 	
@@ -1508,10 +1510,19 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 							delete thisObj.settings.elements[key]['JSEasing'];
 						if(typeof(thisObj.settings.elements[key]["CSS3Easing"])!="undefined")
 							delete thisObj.settings.elements[key]['CSS3Easing'];
+							
+						if(canJQueryAnimate(thisObj.settings.elements[key]["property"]))
+							thisObj.settings.elements[key]['useJQuery'] = true;
+						else{
+							thisObj.settings.elements[key]['useJQuery'] = false;
+							if(typeof(thisObj.JSValuesArray[thisObj.settings.elements[key]["selector"]])=="undefined")
+								thisObj.JSValuesArray[thisObj.settings.elements[key]["selector"]] = {};
+						}
 					}
 					else {
 						if((thisObj.settings.useCSS3 && typeof(thisObj.settings.elements[key]['useCSS3'])=="undefined") || thisObj.settings.elements[key]['useCSS3']){
 							thisObj.settings.elements[key]['useCSS3'] = true;
+							thisObj.settings.elements[key]['useJQuery'] = false;
 							if(typeof(thisObj.settings.elements[key]["easing"])=="undefined"){
 								if(typeof(thisObj.settings.elements[key]["CSS3Easing"])=="undefined")
 									thisObj.settings.elements[key]["easing"] = thisObj.settings.CSS3Easing;
@@ -1546,9 +1557,16 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 								delete thisObj.settings.elements[key]['JSEasing'];
 							if(typeof(thisObj.settings.elements[key]["CSS3Easing"])!="undefined")
 								delete thisObj.settings.elements[key]['CSS3Easing'];
+								
+							if(canJQueryAnimate(thisObj.settings.elements[key]["property"]))
+								thisObj.settings.elements[key]['useJQuery'] = true;
+							else{
+								thisObj.settings.elements[key]['useJQuery'] = false;
+								if(typeof(thisObj.JSValuesArray[thisObj.settings.elements[key]["selector"]])=="undefined")
+									thisObj.JSValuesArray[thisObj.settings.elements[key]["selector"]] = {};
+							}
 						}
 					}
-					(canJQueryAnimate(thisObj.settings.elements[key]["property"]))?thisObj.settings.elements[key]['useJQuery'] = true:thisObj.settings.elements[key]['useJQuery'] = false;
 				}
 
 				/*PUTS PREFIX FOR CSS3*/
@@ -2851,6 +2869,12 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 			isAliveObjects[selector].onComplete = options;
 			return thisObj;
 		},
+		getCurrentPosition : function(thisObj){
+			var selector = thisObj.selector;
+			if(typeof(isAliveObjects[selector])=="undefined")
+				return false;
+			return isAliveObjects[selector].getPos(isAliveObjects[selector].lastStep);
+		},
 		getStepPosition : function(thisObj){
 			var selector = thisObj.selector;
 			if(typeof(isAliveObjects[selector])=="undefined")
@@ -2867,7 +2891,7 @@ Last modification on this file: 24 December 2013 (Merry Christmas)
 			return getBrowser();
 		},
 		getVersion : function(){
-			return "1.7.0";
+			return "1.7.1";
 		}
 	};
 	
