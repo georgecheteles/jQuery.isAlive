a5934132831f17d1aa8e8a2fc00faad7269341ae
georgecheteles <george@we-code-magic.com>
2014-01-05 17:15:00 +0200

    ver. 1.8.3

--- src/jquery.isAlive.js
+++ src/jquery.isAlive.js
@@ -5,14 +5,14 @@
 | |/ |/ /  __/_____/ /__/ /_/ / /_/ /  __/_____/ / / / / / /_/ / /_/ / / /__  
 |__/|__/\___/      \___/\____/\__,_/\___/     /_/ /_/ /_/\__,_/\__, /_/\___/  
                                                               /____/          
-jQuery.isAlive(1.8.2)
+jQuery.isAlive(1.8.3)
 Written by George Cheteles (george@we-code-magic.com).
 Licensed under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license. 
 Please attribute the author if you use it.
 Find me at:
 	http://www.we-code-magic.com 
 	george@we-code-magic.com
-Last modification on this file: 3 January 2014
+Last modification on this file: 5 January 2014
 */
 
 (function(jQuery) {
@@ -22,14 +22,24 @@ Last modification on this file: 3 January 2014
 
 	var isReady = false;
 	var browserObj = null;
-	var vPTransition = null;
-	var vPTransform = null;
 	var indexOf = null;
+	var vPArray = {opacity:"opacity",left:"left",right:"right",top:"top",bottom:"bottom",width:"width",height:"height"};
 	var _fix = {};
-	
+	var canRGBA = false;
+
 	var resizeTimer;
 	var windowWidth;
 	var windowHeight;
+	
+	/*CHECK IF BROWSER SUPPORTS RGBA*/
+	function supportsRGBA(){
+		var el = document.createElement('foo');
+		var prevColor = el.style.color;
+		try {
+			el.style.color = 'rgba(1,5,13,0.44)';
+		} catch(e) {}
+		return el.style.color != prevColor;
+	}
 	
 	/*COMPARES VERSIONS*/
 	function ifVersion(value1,cond,value2){
@@ -107,7 +117,7 @@ Last modification on this file: 3 January 2014
 					else
 						return parseInt(value) - 1;
 				}
-				if(property==vPTransform){
+				if(property==vP('transform')){
 					if(format.indexOf('matrix')==0 || format.indexOf('scale')==0){
 						success = true;
 						return parseFloat(value) + 0.001;
@@ -223,7 +233,7 @@ Last modification on this file: 3 January 2014
 	function nameToRgb(name){
 		var colors={"aliceblue":"240,248,255","antiquewhite":"250,235,215","aqua":"0,255,255","aquamarine":"127,255,212","azure":"240,255,255","beige":"245,245,220","bisque":"255,228,196","black":"0,0,0","blanchedalmond":"255,235,205","blue":"0,0,255","blueviolet":"138,43,226","brown":"165,42,42","burlywood":"222,184,135","cadetblue":"95,158,160","chartreuse":"127,255,0","chocolate":"210,105,30","coral":"255,127,80","cornflowerblue":"100,149,237","cornsilk":"255,248,220","crimson":"220,20,60","cyan":"0,255,255","darkblue":"0,0,139","darkcyan":"0,139,139","darkgoldenrod":"184,134,11","darkgray":"169,169,169","darkgreen":"0,100,0","darkkhaki":"189,183,107","darkmagenta":"139,0,139","darkolivegreen":"85,107,47","darkorange":"255,140,0","darkorchid":"153,50,204","darkred":"139,0,0","darksalmon":"233,150,122","darkseagreen":"143,188,143","darkslateblue":"72,61,139","darkslategray":"47,79,79","darkturquoise":"0,206,209","darkviolet":"148,0,211","deeppink":"255,20,147","deepskyblue":"0,191,255","dimgray":"105,105,105","dodgerblue":"30,144,255","firebrick":"178,34,34","floralwhite":"255,250,240","forestgreen":"34,139,34","fuchsia":"255,0,255","gainsboro":"220,220,220","ghostwhite":"248,248,255","gold":"255,215,0","goldenrod":"218,165,32","gray":"128,128,128","green":"0,128,0","greenyellow":"173,255,47","honeydew":"240,255,240","hotpink":"255,105,180","indianred ":"205,92,92","indigo ":"75,0,130","ivory":"255,255,240","khaki":"240,230,140","lavender":"230,230,250","lavenderblush":"255,240,245","lawngreen":"124,252,0","lemonchiffon":"255,250,205","lightblue":"173,216,230","lightcoral":"240,128,128","lightcyan":"224,255,255","lightgoldenrodyellow":"250,250,210","lightgrey":"211,211,211","lightgreen":"144,238,144","lightpink":"255,182,193","lightsalmon":"255,160,122","lightseagreen":"32,178,170","lightskyblue":"135,206,250","lightslategray":"119,136,153","lightsteelblue":"176,196,222","lightyellow":"255,255,224","lime":"0,255,0","limegreen":"50,205,50","linen":"250,240,230","magenta":"255,0,255","maroon":"128,0,0","mediumaquamarine":"102,205,170","mediumblue":"0,0,205","mediumorchid":"186,85,211","mediumpurple":"147,112,216","mediumseagreen":"60,179,113","mediumslateblue":"123,104,238","mediumspringgreen":"0,250,154","mediumturquoise":"72,209,204","mediumvioletred":"199,21,133","midnightblue":"25,25,112","mintcream":"245,255,250","mistyrose":"255,228,225","moccasin":"255,228,181","navajowhite":"255,222,173","navy":"0,0,128","oldlace":"253,245,230","olive":"128,128,0","olivedrab":"107,142,35","orange":"255,165,0","orangered":"255,69,0","orchid":"218,112,214","palegoldenrod":"238,232,170","palegreen":"152,251,152","paleturquoise":"175,238,238","palevioletred":"216,112,147","papayawhip":"255,239,213","peachpuff":"255,218,185","peru":"205,133,63","pink":"255,192,203","plum":"221,160,221","powderblue":"176,224,230","purple":"128,0,128","red":"255,0,0","rosybrown":"188,143,143","royalblue":"65,105,225","saddlebrown":"139,69,19","salmon":"250,128,114","sandybrown":"244,164,96","seagreen":"46,139,87","seashell":"255,245,238","sienna":"160,82,45","silver":"192,192,192","skyblue":"135,206,235","slateblue":"106,90,205","slategray":"112,128,144","snow":"255,250,250","springgreen":"0,255,127","steelblue":"70,130,180","tan":"210,180,140","teal":"0,128,128","thistle":"216,191,216","tomato":"255,99,71","turquoise":"64,224,208","violet":"238,130,238","wheat":"245,222,179","white":"255,255,255","whitesmoke":"245,245,245","yellow":"255,255,0","yellowgreen":"154,205,50"};
 		if(typeof(colors[name.toLowerCase()])!="undefined"){
-			if(!browserObj.msie || (browserObj.msie && parseInt(browserObj.version)>=9))
+			if(canRGBA)
 				return "rgba("+colors[name.toLowerCase()]+",1)";
 			else
 				return "rgb("+colors[name.toLowerCase()]+")";
@@ -239,7 +249,7 @@ Last modification on this file: 3 January 2014
 		});
 		var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 		if(result){
-			if(!browserObj.msie || (browserObj.msie && parseInt(browserObj.version)>=9))
+			if(canRGBA)
 				return "rgba("+parseInt(result[1],16)+","+parseInt(result[2],16)+","+parseInt(result[3], 16)+",1)";
 			else
 				return "rgb("+parseInt(result[1],16)+","+parseInt(result[2],16)+","+parseInt(result[3], 16)+")";
@@ -356,39 +366,36 @@ Last modification on this file: 3 January 2014
 
 	/*MAKES THE CSS CHANGES FOR EACH BROWSER*/
 	function vP(property){
-		if(indexOf(['opacity','scrollTop','scrollLeft'],property)!=-1 || property.indexOf('F:')==0)
-			return property;
-		var el = document.createElement('foo');
-		var p = propFirstLowCase(property);
-		if(typeof(el.style[p])!="undefined")
-			return property;
-		if(property.indexOf('-webkit-')==0 || property.indexOf('-moz-')==0 || property.indexOf('-ms-')==0 || property.indexOf('-o-')==0)
-			return false;
-		if(browserObj.webkit){
-			var v = propFirstLowCase("-webkit-"+property);
-			if(typeof(el.style[v])!="undefined")
-				return "-webkit-"+property;
-			return false;
+		if(typeof(vPArray[property])=="undefined"){
+			vPArray[property] = false;
+			var el = document.createElement('foo');
+			var p = propFirstLowCase(property);
+			if(typeof(el.style[p])!="undefined")
+				vPArray[property] = property;
+			else if(browserObj.webkit){
+				var v = propFirstLowCase("-webkit-"+property);
+				if(typeof(el.style[v])!="undefined")
+					vPArray[property] = "-webkit-"+property;
+			}
+			else if(browserObj.mozilla){
+				var v = propFirstUpCase("-moz-"+property);
+				if(typeof(el.style[v])!="undefined")
+					vPArray[property] = "-moz-"+property;
+			}
+			else if(browserObj.msie){
+				var v = propFirstLowCase("-ms-"+property);
+				if(typeof(el.style[v])!="undefined")
+					vPArray[property] = "-ms-"+property;
+			}
+			else if(browserObj.opera){
+				var v = propFirstUpCase("-o-"+property);
+				if(typeof(el.style[v])!="undefined")
+					vPArray[property] = "-o-"+property;
+			}
+			else 
+				vPArray[property] = property;
 		}
-		if(browserObj.mozilla){
-			var v = propFirstUpCase("-moz-"+property);
-			if(typeof(el.style[v])!="undefined")
-				return "-moz-"+property;
-			return false;
-		}
-		if(browserObj.msie){
-			var v = propFirstLowCase("-ms-"+property);
-			if(typeof(el.style[v])!="undefined")
-				return "-ms-"+property;
-			return false;
-		}
-		if(browserObj.opera){
-			var v = propFirstUpCase("-o-"+property);
-			if(typeof(el.style[v])!="undefined")
-				return "-o-"+property;
-			return false;
-		}
-		return property;
+		return vPArray[property];
 	}
 	
 	/*CALCULATE THE CSS PROPERTY AT A POSITION*/
@@ -563,7 +570,7 @@ Last modification on this file: 3 January 2014
 			delete thisObj.JSValuesArray[selector][property];
 		/*SET TRANSITION AND BREAK CSS3 BUG IF NEEDED*/
 		thisObj.CSS3TransitionArray[selector][property] = property+' '+duration+'ms '+easing+' 0s';
-		jQuery(selector).css(vPTransition,thisObj.getTransitionArray(selector));
+		jQuery(selector).css(vP('transition'),thisObj.getTransitionArray(selector));
 		if(typeof(thisObj.CSS3ValuesArray[selector][property])!="undefined" && thisObj.CSS3ValuesArray[selector][property]==value)
 			value = breakCSS3(property,value);
 		thisObj.CSS3ValuesArray[selector][property] = value;
@@ -603,14 +610,14 @@ Last modification on this file: 3 January 2014
 			jQuery(selector).scrollLeft(value);
 			return;
 		}
-		if(property==vPTransition){
+		if(property==vP('transition')){
 			jQuery(selector).css(property,thisObj.getTransitionArray(selector,value));
 			return;
 		}
 		if(typeof(thisObj.CSS3TransitionArray[selector])!="undefined" && typeof(thisObj.CSS3TransitionArray[selector][property])!="undefined"){
 			delete thisObj.CSS3TransitionArray[selector][property];
 			delete thisObj.CSS3ValuesArray[selector][property];
-			jQuery(selector).css(vPTransition,thisObj.getTransitionArray(selector));
+			jQuery(selector).css(vP('transition'),thisObj.getTransitionArray(selector));
 		}
 		else if(typeof(thisObj.JSValuesArray[selector])!="undefined" && typeof(thisObj.JSValuesArray[selector][property])!="undefined")
 			thisObj.JSValuesArray[selector][property] = value;
@@ -648,7 +655,7 @@ Last modification on this file: 3 January 2014
 				var format = params.substr(0,params.indexOf('('));
 				if(format=="url")
 					return params;
-				if(format=='rgb' && (!browserObj.msie || (browserObj.msie && parseInt(browserObj.version)>=9)))
+				if(format=='rgb' && canRGBA)
 					return 'rgba(' + doRecursive(getBetweenBrackets(params)) + ',1)';
 				return format + '(' + doRecursive(getBetweenBrackets(params)) + ')';
 			}
@@ -698,7 +705,7 @@ Last modification on this file: 3 January 2014
 				for (var key in tempArray){
 					if(tempArray[key].indexOf(')')!=-1){
 						tempArray[key] = tempArray[key].split(')');
-						tempArray[key][0] = tempArray[key][0].replace(/,/g,'$');
+						tempArray[key][0] = tempArray[key][0].replace(/,/g,'*CHAR*');
 						tempArray[key] = tempArray[key].join(')'); 
 					}
 				}
@@ -706,8 +713,9 @@ Last modification on this file: 3 January 2014
 			}
 			for(key in tempArray){
 				var temp = tempArray[key].split(' ');
-				var fix = vP(temp[0]);
-				thisObj.CSS3DefaultTransitionArray[selector][fix] = tempArray[key].replace(temp[0],fix).replace(/$/g,","); 
+				var property = vP(temp[0]);
+				if(property!=false)
+					thisObj.CSS3DefaultTransitionArray[selector][property] = tempArray[key].replace(temp[0],property).replace(/\*CHAR\*/g,","); 
 			}
 		}
 
@@ -1300,16 +1308,16 @@ Last modification on this file: 3 January 2014
 			browserObj = getBrowser();
 			
 			/* ARRAY SEARCH FOR IE7&IE8 FIX*/
-			if(!browserObj.msie || (browserObj.msie && parseInt(browserObj.version)>=9)){
-				indexOf = function (myArray,myValue){
-					return myArray.indexOf(myValue);
-				}
-			}else{
+			if(!Array.prototype.indexOf){
 				indexOf = function (myArray,myValue){
 					for(var key in myArray)
 						if(myArray[key]==myValue)
 							return parseInt(key);
 					return -1;	
+				}
+			}else{
+				indexOf = function (myArray,myValue){
+					return myArray.indexOf(myValue);
 				}
 			}
 			
@@ -1318,16 +1326,17 @@ Last modification on this file: 3 January 2014
 			windowHeight = jQuery(window).height();
 			jQuery(window).bind('resize',onResizeAction);
 			
-			/*SET TRASITION WITH PREFIX VENTOR*/ 
-			vPTransition = vP('transition');
-			vPTransform = vP('transform');
-			
 			/*ADDED VALUE FOR CSS BUG FIXED*/
 			_fix['opacity'] = (browserObj.opera) ? 0.004 : 0.001;
 			_fix['px'] = (browserObj.opera || (browserObj.safari && !browserObj.mobile && ifVersion(browserObj.version,'<','6.0.0'))) ? 1 : 0.01;
 			_fix['%'] = 0.01;
 			_fix['deg'] = 0.01;
 			_fix['em'] = (browserObj.opera || (browserObj.safari && !browserObj.mobile && ifVersion(browserObj.version,'<','6.0.0'))) ? 0.1 : 0.01;
+			
+			/*GETS READY SOME CSS3 PROP & CHECK IF RGBA IS SUPPORTED*/
+			vP('transition');
+			vP('transform');
+			canRGBA = supportsRGBA();
 		}
 		
 		/*TIMELINE WRAPPER*/
@@ -1420,6 +1429,11 @@ Last modification on this file: 3 January 2014
 		var keyIndex = 0;
 		var tempArray = [];
 		for(key in thisObj.settings.elements){
+			/*IF NO SELECTOR IS FOUND*/
+			if(typeof(thisObj.settings.elements[key]['selector'])=="undefined"){
+				delete thisObj.settings.elements[key];
+				continue;
+			}
 			/*DELETE ELEMENTS FOR OTHER BROWSERS THEN MINE*/
 			if(typeof(thisObj.settings.elements[key]['+browsers'])!="undefined"){
 				if(!validateBrowsers(thisObj.settings.elements[key]['+browsers'])){
@@ -1538,7 +1552,7 @@ Last modification on this file: 3 January 2014
 						thisObj.functionsArray[fTemp] = thisObj.settings.elements[key]['property'];
 					thisObj.settings.elements[key]['property'] = fTemp;
 				}
-				else{
+				else if(thisObj.settings.elements[key]['property']!='scrollTop' && thisObj.settings.elements[key]['property']!='scrollLeft'){
 					/*PUTS PREFIX FOR CSS3*/
 					thisObj.settings.elements[key]['property'] = vP(thisObj.settings.elements[key]['property']);
 					if(thisObj.settings.elements[key]['property']==false){
@@ -1559,7 +1573,7 @@ Last modification on this file: 3 January 2014
 				
 				/*SET CSS3 VARS*/
 				if(thisObj.settings.elements[key]["method"]=="animate"){
-					if((browserObj.msie && parseInt(browserObj.version)<10) || thisObj.settings.elements[key]['property']=='scrollTop' || thisObj.settings.elements[key]['property']=='scrollLeft' || thisObj.settings.elements[key]["property"].indexOf('F:')==0 || (!thisObj.settings.useCSS3 && typeof(thisObj.settings.elements[key]['useCSS3'])=="undefined") || thisObj.settings.elements[key]['useCSS3']==false){
+					if(vP('transition')==false || thisObj.settings.elements[key]['property']=='scrollTop' || thisObj.settings.elements[key]['property']=='scrollLeft' || thisObj.settings.elements[key]["property"].indexOf('F:')==0 || (!thisObj.settings.useCSS3 && typeof(thisObj.settings.elements[key]['useCSS3'])=="undefined") || thisObj.settings.elements[key]['useCSS3']==false){
 						/*BUILD JS ARRAY*/
 						if(indexOf(jQueryCanAnimate,propFirstLowCase(thisObj.settings.elements[key]["property"]))!=-1)
 							thisObj.settings.elements[key]['animType'] = 1;
@@ -1616,6 +1630,7 @@ Last modification on this file: 3 January 2014
 				}
 			}
 		}
+		
 		/*CHECKS IF ENABLE GPU IS VALID AND ADD SPECIAL CSS*/
 		if(browserObj.webkit && (thisObj.settings.enableGPU==true || (thisObj.settings.enableGPU!=false && validateBrowsers(thisObj.settings.enableGPU))))
 			jQuery('.'+thisObj.settings.animateClass).css({
@@ -1626,7 +1641,7 @@ Last modification on this file: 3 January 2014
 		var tempArray = [];
 		for(key in thisObj.settings.elements){
 			/* CREATES ARRAY WITH TRANSITIONS CSS VALUES*/
-			if(thisObj.settings.elements[key]['animType']==3 && (!browserObj.msie || (browserObj.msie && parseInt(browserObj.version)>9)))
+			if(thisObj.settings.elements[key]['animType']==3 && vP('transition')!=false)
 				if(typeof(thisObj.CSS3DefaultTransitionArray[thisObj.settings.elements[key]['selector']])=="undefined"){
 					var propTempArray = [];
 					var pTemp1 = jQuery(thisObj.settings.elements[key]['selector']).css(vP('transition-property'));
@@ -1665,7 +1680,7 @@ Last modification on this file: 3 January 2014
 					for (var keyA in aTemp){
 						if(aTemp[keyA].indexOf(')')!=-1){
 							aTemp[keyA] = aTemp[keyA].split(')');
-							aTemp[keyA][0] = aTemp[keyA][0].replace(/,/g,'$');
+							aTemp[keyA][0] = aTemp[keyA][0].replace(/,/g,'*CHAR*');
 							aTemp[keyA] = aTemp[keyA].join(')'); 
 						}
 					}
@@ -1679,7 +1694,7 @@ Last modification on this file: 3 January 2014
 				transVal.push(jQuery.trim(tempArray[1][key2]));
 				transVal.push(jQuery.trim(tempArray[2][key2]));
 				transVal.push(jQuery.trim(tempArray[3][key2]));
-				thisObj.CSS3DefaultTransitionArray[key][jQuery.trim(tempArray[0][key2])] = transVal.join(" ").replace(/$/g,',');
+				thisObj.CSS3DefaultTransitionArray[key][jQuery.trim(tempArray[0][key2])] = transVal.join(" ").replace(/\*CHAR\*/g,',');
 			}
 		}
 		
@@ -2050,7 +2065,7 @@ Last modification on this file: 3 January 2014
 				for(selector in thisObj.CSS3TransitionArray){
 					thisObj.CSS3TransitionArray[selector] = {};
 					thisObj.CSS3ValuesArray[selector] = {};
-					jQuery(selector).css(vPTransition,thisObj.getTransitionArray(selector));
+					jQuery(selector).css(vP('transition'),thisObj.getTransitionArray(selector));
 				}
 				
 				if(thisObj.rebuildOnStop){
@@ -2622,7 +2637,7 @@ Last modification on this file: 3 January 2014
 				CSSValues[property] = jQuery(selector).css(property);
 			thisObj.CSS3TransitionArray[selector] = {};
 			thisObj.CSS3ValuesArray[selector] = {};
-			jQuery(selector).css(vPTransition,thisObj.getTransitionArray(selector));
+			jQuery(selector).css(vP('transition'),thisObj.getTransitionArray(selector));
 			jQuery(selector).css(CSSValues);
 		}
 	}
@@ -2877,7 +2892,7 @@ Last modification on this file: 3 January 2014
 			return getBrowser();
 		},
 		getVersion : function(){
-			return "1.8.2";
+			return "1.8.3";
 		}
 	};
 	
