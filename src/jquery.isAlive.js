f6dfaf57d7400cab59bdd8989aceca0fbbf04d87
George <georgecheteles@reea.net>
2014-02-05 16:23:37 +0200

    ver. 1.9.11

--- src/jquery.isAlive.js
+++ src/jquery.isAlive.js
@@ -5,14 +5,14 @@
 | |/ |/ /  __/_____/ /__/ /_/ / /_/ /  __/_____/ / / / / / /_/ / /_/ / / /__  
 |__/|__/\___/      \___/\____/\__,_/\___/     /_/ /_/ /_/\__,_/\__, /_/\___/  
                                                               /____/          
-jQuery.isAlive(1.9.10)
+jQuery.isAlive(1.9.11)
 Written by George Cheteles (george@we-code-magic.com).
 Licensed under the MIT (https://github.com/georgecheteles/jQuery.isAlive/blob/master/MIT-LICENSE.txt) license. 
 Please attribute the author if you use it.
 Find me at:
 	http://www.we-code-magic.com 
 	george@we-code-magic.com
-Last modification on this file: 24 January 2014
+Last modification on this file: 5 February 2014
 */
 
 (function(jQuery) {
@@ -26,7 +26,7 @@ Last modification on this file: 24 January 2014
 	var vPArray = {opacity:"opacity",left:"left",right:"right",top:"top",bottom:"bottom",width:"width",height:"height"};
 	var _fix = {};
 	var canRGBA = false;
-	var MSPointerEnabled,MSPointerDown,MSPointerMove,MSPointerUp,MSMaxTouchPoints;
+	var MSPointerEnabled,MSPointerDown,MSPointerMove,MSPointerUp,MSMaxTouchPoints,WheelEvent;
 	
 	var resizeOn = false;
 	var resizeTimer;
@@ -340,7 +340,7 @@ Last modification on this file: 24 January 2014
 		var mobile = (/(android|bb\d+|meego).+mobile|webos|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4)));
 		if(mobile)
 			browser.mobile = true;
-		browser.isTouch = ('ontouchstart' in document.documentElement) || ((window.navigator.maxTouchPoints || window.navigator.msMaxTouchPoints) ? true : false);
+		browser.isTouch = ('ontouchstart' in document.documentElement || window.navigator.maxTouchPoints || window.navigator.msMaxTouchPoints) ? true : false;
 		return browser;
 	}
 	
@@ -917,34 +917,16 @@ Last modification on this file: 24 January 2014
 	isAlive.prototype.bindScrollTouchEvents = function(){
 		
 		var thisObj = this;
-		
+
 		/* BIND SCROLL EVENTS */
 		if(thisObj.settings.enableWheel){
-			var wheelEvent = (browserObj.mozilla) ? "MozMousePixelScroll.wheel" : "DOMMouseScroll.wheel";
-			if(thisObj.settings.wheelType=="scroll"){
-				jQuery(thisObj.mySelector).bind('mousewheel.wheel',function(e){
-					(e.originalEvent.wheelDelta < 0) ? thisObj.doScroll(thisObj.settings.wheelActions.down) : thisObj.doScroll(thisObj.settings.wheelActions.up);
-					if(thisObj.settings.preventWheel)
-						return false;
-				});
-				jQuery(thisObj.mySelector).bind(wheelEvent,function(e){
-					(e.originalEvent.detail > 0) ? thisObj.doScroll(thisObj.settings.wheelActions.down) : thisObj.doScroll(thisObj.settings.wheelActions.up);
-					if(thisObj.settings.preventWheel)
-						return false;
-				});
-			}
-			else{
-				jQuery(thisObj.mySelector).bind('mousewheel.wheel',function(e){
-					(e.originalEvent.wheelDelta < 0) ? thisObj.doJump(thisObj.settings.wheelActions.down) : thisObj.doJump(thisObj.settings.wheelActions.up);
-					if(thisObj.settings.preventWheel)
-						return false;
-				});
-				jQuery(thisObj.mySelector).bind(wheelEvent,function(e){
-					(e.originalEvent.detail > 0) ? thisObj.doJump(thisObj.settings.wheelActions.down) : thisObj.doJump(thisObj.settings.wheelActions.up);
-					if(thisObj.settings.preventWheel)
-						return false;
-				});
-			}
+			jQuery(thisObj.mySelector).bind(WheelEvent+'.isAlive',function(e){
+				var wheelValue;
+				(e.type=='wheel') ? wheelValue = (e.originalEvent.deltaY > 0) : ((e.type=='mousewheel') ? wheelValue = (e.originalEvent.wheelDelta < 0) : wheelValue = (e.originalEvent.detail > 0)); 
+				(thisObj.settings.wheelType=="scroll") ? ((wheelValue) ? thisObj.doScroll(thisObj.settings.wheelActions.down) : thisObj.doScroll(thisObj.settings.wheelActions.up)) : ((wheelValue) ? thisObj.doJump(thisObj.settings.wheelActions.down) : thisObj.doJump(thisObj.settings.wheelActions.up));
+				if(thisObj.settings.preventWheel)
+					return false;
+			});
 		}
 		
 		/* BIND TOUCH EVENTS */
@@ -1084,13 +1066,13 @@ Last modification on this file: 24 January 2014
 			}
 			
 			if(MSPointerEnabled && MSMaxTouchPoints){
-				jQuery(thisObj.mySelector).bind(MSPointerDown+'.touch',onTouchStart);
+				jQuery(thisObj.mySelector).bind(MSPointerDown+'.isAlive',onTouchStart);
 				/*PREVENT TOUCH EVENTS*/
 				if(thisObj.settings.preventTouch)
 					jQuery(thisObj.mySelector).addClass('isalive-notouchaction');
 			}
 			if('ontouchstart' in document.documentElement)
-				jQuery(thisObj.mySelector).bind('touchstart.touch',onTouchStart);
+				jQuery(thisObj.mySelector).bind('touchstart.isAlive',onTouchStart);
 		}
 		
 	}
@@ -1336,6 +1318,9 @@ Last modification on this file: 24 January 2014
 			MSPointerMove = (window.navigator.msPointerEnabled || window.navigator.pointerEnabled) ? ((window.navigator.pointerEnabled) ? "pointermove" : "MSPointerMove") : false;
 			MSPointerUp = (window.navigator.msPointerEnabled || window.navigator.pointerEnabled) ? ((window.navigator.pointerEnabled) ? "pointerup" : "MSPointerUp") : false;
 			MSMaxTouchPoints = (window.navigator.msPointerEnabled || window.navigator.pointerEnabled) ? (window.navigator.maxTouchPoints || window.navigator.msMaxTouchPoints) : false;
+			
+			/*GET WHEEL EVENT*/
+			WheelEvent = "onwheel" in document.createElement("div") ? "wheel" : ((typeof(document.onmousewheel) !== "undefined") ? "mousewheel" : ((browserObj.mozilla) ? "MozMousePixelScroll" : "DOMMouseScroll"));
 		}
 		
 		/*BINDS RESIZE EVENT*/
@@ -2807,16 +2792,10 @@ Last modification on this file: 24 January 2014
 	isAlive.prototype.destroy = function(){
 		var thisObj = this;
 		/*UNBIND EVENTS*/
-		jQuery(thisObj.mySelector).unbind('mousewheel.wheel');
-		jQuery(thisObj.mySelector).unbind('MozMousePixelScroll.wheel');
-		jQuery(thisObj.mySelector).unbind('DOMMouseScroll.wheel');
-		jQuery(thisObj.mySelector).unbind('touchstart.touch');
-		jQuery(thisObj.mySelector).unbind(MSPointerDown+'.touch');
+		jQuery(thisObj.mySelector).unbind('.isAlive');
 		for(var key in thisObj.settings.elements){
 			if(thisObj.settings.elements[key]['scrollbar']){
-				jQuery(thisObj.settings.elements[key]['selector']).unbind(MSPointerDown+'.scrollbar');
-				jQuery(thisObj.settings.elements[key]['selector']).unbind('mousedown.scrollbar');
-				jQuery(thisObj.settings.elements[key]['selector']).unbind('touchstart.scrollbar');
+				jQuery(thisObj.settings.elements[key]['selector']).unbind('.scrollbar');
 				jQuery(thisObj.settings.elements[key]['selector']).removeAttr('unselectable');
 				jQuery(thisObj.settings.elements[key]['selector']).removeClass('isalive-nouserselect');
 				jQuery(thisObj.settings.elements[key]['selector']).removeClass('isalive-notouchaction');
@@ -2977,7 +2956,7 @@ Last modification on this file: 24 January 2014
 			return getBrowser();
 		},
 		getVersion : function(){
-			return "1.9.10";
+			return "1.9.11";
 		}
 	};
 	
